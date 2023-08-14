43ee863c3e344a308332d13ee90a88c612440b62
Priiyyya-21 <priyamajhi79@gmail.com>
2023-08-15 00:28:17 +0530

    Created single news component

--- NewsNow/components/SingleNews.js
+++ NewsNow/components/SingleNews.js
@@ -1,11 +1,10 @@
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image, ImageBackground ,TouchableOpacity,Linking} from 'react-native'
 import React from 'react'
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const SingleNews = ({ item, index }) => {
-
-    const windowWidth = Dimensions.get("window").width;
-    const windowHeight = Dimensions.get("window").height;
 
     return (
         <View style={{
@@ -19,31 +18,60 @@ const SingleNews = ({ item, index }) => {
             />
             <View style={{ ...styles.description, backgroundColor: "#282C35" }}>
 
-           
-            <Text style={{ ...styles.title, color: "white" }}>{item.title}</Text>
 
-            <Text style={{ ...styles.content, color: "white" }}>
-                {item.description}
-            </Text>
+                <Text style={{ ...styles.title, color: "white" }}>{item.title}</Text>
+
+                <Text style={{ ...styles.content, color: "white" }}>
+                    {item.description}
+                </Text>
+
+                <Text style={{ color: "white" }}>Short by
+                    <Text> {item.author ?? "unknown"}</Text>
+                </Text>
+
+                <ImageBackground
+                    blurRadius={30}
+                    style={styles.footer}
+                    source={{ uri: item.urlToImage }}
+                >
+                    <TouchableOpacity onPress={() => Linking.openURL(item.url)}>
+                        <Text style={{ fontSize: 15, color: "white" }}>
+                            '{item?.content?.slice(0, 45)}...'
+                        </Text>
+                        <Text style={{ fontSize: 17, fontWeight: "bold", color: "white" }}>
+                            Read More
+                        </Text>
+                    </TouchableOpacity>
+                </ImageBackground>
             </View>
 
         </View>
     )
 }
 
-export default SingleNews
+export default SingleNews;
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 25,
         fontWeight: "bold",
-        // marginTop:60,
-        marginLeft: 7,
         paddingBottom: 10,
     },
     content: {
         fontSize: 18,
         paddingBottom: 10,
-        // alignItems:"center",
     },
+    footer: {
+        height: 80,
+        width: windowWidth,
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: "#d7be69",
+        justifyContent: "center",
+        paddingHorizontal: 20,
+      },
+      description: {
+        padding: 15,
+        flex: 1,
+      },
 });
