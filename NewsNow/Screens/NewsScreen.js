61ea5f93caa692c8bb72651ddc64b64ef964dde4
Priiyyya-21 <priyamajhi79@gmail.com>
2023-08-14 22:01:35 +0530

    Created NewsScreen tab view

--- NewsNow/Screens/NewsScreen.js
+++ NewsNow/Screens/NewsScreen.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import React, { useContext,useState } from 'react'
+import { NewsContext } from '../API/Context'
+import { Dimensions } from 'react-native'
+import Carousel from 'react-native-snap-carousel'
 
 const NewsScreen = () => {
+
+  const {
+    news: { articles },
+  } = useContext(NewsContext)
+
+  const [activeIndex, setActiveIndex] = useState();
+  const windowHeight = Dimensions.get("window").height;
+
+
   return (
-    <View>
-      <Text>NewsScreen</Text>
+    <View style={styles.carousel}>
+      {articles && (
+        <Carousel
+          // firstItem={articles.slice(0, 10).length - 1}
+          layout={"stack"}
+          data={articles.slice(0, 10)}
+          sliderHeight={300}
+          itemHeight={windowHeight}
+          vertical={true}
+          // renderItem={({ item, index }) => (
+          //   <SingleNews item={item} index={index} darkTheme={darkTheme} />
+          // )}
+          onSnapToItem={(index) => setActiveIndex(index)}
+        /> 
+      )}
     </View>
   )
 }
 
 export default NewsScreen
+
+const styles = StyleSheet.create({
+  carousel: {
+    flex: 1,
+    transform: [{ scaleY: -1 }],
+    backgroundColor: "black",
+  },
+});
