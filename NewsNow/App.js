fd182f91ba6d16ca1bedeaf0f1c2c43807c9d1c4
Priiyyya-21 <priyamajhi79@gmail.com>
2023-08-14 14:51:34 +0530

    Added header section

--- NewsNow/App.js
+++ NewsNow/App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InshortTabs from './components/InshortTabs';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HI!</Text>
-      <StatusBar style="auto" />
+  return <View style={styles.container}>
+    <InshortTabs/>
+
+    <StatusBar style="auto" />
     </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:50,
+    // marginTop:StatusBar.currentHeight,
   },
 });
